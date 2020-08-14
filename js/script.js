44fc74662367c78fd04623256ab85739a32586bb
gusehdqo <67905177+gusehdqo@users.noreply.github.com>
2020-08-14 16:56:56 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -50,20 +50,20 @@
 //   swiper06Thumb.params.control = swiper06Main;
 // }, false);
 
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-      spaceBetween: 10,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesVisibility: true,
-      watchSlidesProgress: true,
-    });
-    var galleryTop = new Swiper('.gallery-top', {
-      spaceBetween: 10,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      thumbs: {
-        swiper: galleryThumbs
-      }
-    });
+// var galleryThumbs = new Swiper('.gallery-thumbs', {
+//       spaceBetween: 10,
+//       slidesPerView: 4,
+//       freeMode: true,
+//       watchSlidesVisibility: true,
+//       watchSlidesProgress: true,
+//     });
+//     var galleryTop = new Swiper('.gallery-top', {
+//       spaceBetween: 10,
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//       thumbs: {
+//         swiper: galleryThumbs
+//       }
+//     });
